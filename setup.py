2331277ab13159bfdbbdb2cb2de1ea45c1c7d33f
luyomo <syunka.tyo@gmail.com>
2021-04-19 15:46:25 +0900

    Improved the __init__.py for easy import

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 setup(
   name = 'yomo-py-common',
-  packages = ['yomoPyCommon'],
-  version = '0.0.1',
+  packages = ['yomoPyCommon', 'yomoPyCommon/db', 'yomoPyCommon/message'],
+  include_package_data=True,
+  version = '0.0.5',
   license='MIT',
   description = 'Define some common function for easy use',
   author = 'Chunhua Zhang',
   author_email = 'syunka.tyo@gmail.com',
   url = 'https://github.com/luyomo/yomo-py-common',
-  download_url = 'https://github.com/luyomo/yomo-py-common/archive/refs/tags/0.0.1.tar.gz',
+  download_url = 'https://github.com/luyomo/yomo-py-common/archive/refs/tags/0.0.5.tar.gz',
   keywords = ['postgres', 'wechat'] ,
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
